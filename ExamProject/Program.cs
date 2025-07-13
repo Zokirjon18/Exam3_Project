bfcca4b53356ccddf6685bee103c5b2261b95e69
Zokirjon18 <tolqinovzokirjon347@gmail.com>
2025-07-13 17:31:39 +0500

    Add project files.

---
+++ ExamProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Exam!");
+        }
+    }
+}
