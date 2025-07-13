a307a3335ad89582af817dde3dce0c54146d5381
Zokirjon18 <tolqinovzokirjon347@gmail.com>
2025-07-13 22:15:32 +0500

    dynamic file pathlar yaratildi

---
+++ ExamProject/Constants/PathHolder.cs
@@ -0,0 +1,9 @@
+﻿namespace ExamProject.Constants
+{
+    internal class PathHolder
+    {
+        private static readonly string parentRoot = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+        public static readonly string CategoryPath = Path.Combine(parentRoot, "Data", "categorys.txt");
+        public static readonly string DishPath = Path.Combine(parentRoot, "Data", "dishes.txt");
+    }
+}
